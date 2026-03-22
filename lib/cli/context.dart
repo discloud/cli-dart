0a8e8d46d06ccfab3d519a73720c316163b2225c
Gorniaky <hwss.s2@gmail.com>
2026-03-21 23:44:15 -0300

    add elapsed time

--- lib/cli/context.dart
+++ lib/cli/context.dart
@@ -5,7 +5,9 @@ import "package:discloud/constants.dart";
 import "package:discloud/extensions/list.dart";
 import "package:discloud/services/discloud/api_client.dart";
 import "package:discloud/structures/disposable.dart";
+import "package:duration/duration.dart";
 import "package:local_store/local_store.dart";
+import "package:tint/tint.dart";
 
 abstract class CliContext implements Disposable {
   static final CliContext I = _CliContext();
@@ -15,6 +17,8 @@ abstract class CliContext implements Disposable {
     _arguments = arguments;
     _debug = arguments.contains("--debug");
   }
+
+  final Stopwatch _stopwatch = .new()..start();
 
   DateTime? _start;
   DateTime get start => _start ??= .now();
@@ -37,8 +41,16 @@ abstract class CliContext implements Disposable {
 
   @override
   Future<void> dispose() async {
+    _stopwatch.stop();
     api.dispose();
     await subscriptions.dispose();
+
+    final elapsed = _stopwatch.elapsed.pretty(
+      abbreviated: true,
+      tersity: .millisecond,
+    ).replaceAll(",", "");
+
+    stderr.writeln("Elapsed: $elapsed".dim());
   }
 }
 
