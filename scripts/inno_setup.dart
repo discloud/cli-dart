36f91ef0eac60001aa7bbc6ebfa4a9de5e35421e
Gorniaky <hwss.s2@gmail.com>
2026-03-22 22:07:21 -0300

    refactor: update inno_setup script and sync workflow
    - Make version option mandatory in inno_setup.dart
    - Add pubspec.yaml version synchronization to the script
    - Use pub_semver for version validation
    - Move version sync step earlier in build_release workflow
    - Add pub_semver to dev_dependencies

--- scripts/inno_setup.dart
+++ scripts/inno_setup.dart
@@ -2,25 +2,33 @@ import "dart:io";
 
 import "package:args/args.dart";
 import "package:interpolation/interpolation.dart";
-import "package:yaml/yaml.dart";
+import "package:pub_semver/pub_semver.dart";
 
 void main(Iterable<String> args) async {
-  final interpolator = Interpolation(
-    option: .new(prefix: "{{", suffix: "}}"),
-  );
-
-  final parser = ArgParser()..addOption("version");
+  final parser = ArgParser()..addOption("version", mandatory: true);
   final parsed = parser.parse(args);
+  final version = parsed.option("version")!;
+  Version.parse(version);
 
   final issFile = File("inno_setup.iss");
   final pubspecFile = File("pubspec.yaml");
 
-  String issContent = await issFile.readAsString();
-  final pubspec = loadYaml(await pubspecFile.readAsString());
+  final interpolator = Interpolation(
+    option: .new(prefix: "{{", suffix: "}}"),
+  );
 
-  final version = parsed.option("version") ?? pubspec["version"];
+  String issContent = await issFile.readAsString();
+  String pubspecContent = await pubspecFile.readAsString();
 
   issContent = interpolator.eval(issContent, {"version": version});
+  pubspecContent = pubspecContent.replaceFirst(
+    pubspecVersionRegexp,
+    "\nversion: $version\n",
+  );
 
   await issFile.writeAsString(issContent);
+  await pubspecFile.writeAsString(pubspecContent);
 }
+
+const pubspecVersionPattern = r"\r?\nversion:\s\d+\.\d+\.\d+\r?\n";
+final pubspecVersionRegexp = RegExp(pubspecVersionPattern);
