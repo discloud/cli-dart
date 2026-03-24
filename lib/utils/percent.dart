ffc1f8bbdbfcda7095c88cf9ff6c18035c3f99fd
Gorniaky <hwss.s2@gmail.com>
2026-03-24 18:25:49 -0300

    change percent return to string

--- lib/utils/percent.dart
+++ lib/utils/percent.dart
@@ -1,3 +1,3 @@
-double percent(num actual, num total, [int precision = 2]) {
-  return .parse((actual / total * 100).toStringAsPrecision(precision));
+String percent(num actual, num total, [int precision = 2]) {
+  return (actual / total * 100).toStringAsPrecision(precision);
 }
