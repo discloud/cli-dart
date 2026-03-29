485ea9414b461dd6a5751ca38c130eac0e6656e8
Gorniaky <hwss.s2@gmail.com>
2026-03-29 18:01:06 -0300

    improve init command

--- lib/commands/init.dart
+++ lib/commands/init.dart
@@ -1,6 +1,7 @@
 import "dart:async";
 import "dart:io";
 
+import "package:args/args.dart";
 import "package:args/command_runner.dart";
 import "package:discloud/extensions/string.dart";
 import "package:discloud/services/discloud/constants.dart";
@@ -46,35 +47,54 @@ class InitCommand extends Command<void> {
   Future<void> run() async {
     final file = File(DiscloudConfig.filename);
 
-    if (!argResults!.flag("force") && await file.exists()) {
+    final args = _InitArgs(argResults);
+
+    if (!args.force && await file.exists()) {
       throw Exception("${DiscloudConfig.filename} already exists!");
     }
-
-    final args = argResults;
-
-    final hostname = args?.option("hostname");
 
     final buffer = StringBuffer()
       ..writeAll([
         "# https://docs.discloud.com/en/discloud.config",
-        if (args?.multiOption("apt") case final v? when v.isNotEmpty)
-          "APT=${v.join(",")}",
-        if (args?.flag("autorestart") case final v? when v) "AUTORESTART=$v",
-        if (args?.option("avatar") case final v?) "AVATAR=$v",
-        if (hostname case final v?) "HOSTNAME=$v",
-        if (args?.option("id") case final v?) "ID=$v",
-        "MAIN=${args?.option("main").orEmpty}",
-        if (args?.option("name") case final v?) "NAME=$v",
-        if (args?.option("ram") case final v?) "RAM=$v",
-        if (args?.option("type") case final v?) "TYPE=$v",
-        if (args?.option("version") case final v?) "VERSION=$v",
-        if (args?.flag("vlan") case final v?
-            when v || hostname is String && hostname.isNotEmpty)
-          "VLAN=$v",
+        if (args.apt case final v when v.isNotEmpty) "APT=${v.join(",")}",
+        if (args.autorestart case final v when v) "AUTORESTART=$v",
+        if (args.avatar case final v?) "AVATAR=$v",
+        if (args.hostname case final v?) "HOSTNAME=$v",
+        if (args.id case final v?) "ID=$v",
+        "MAIN=${args.main.orEmpty}",
+        if (args.name case final v?) "NAME=$v",
+        if (args.ram case final v?) "RAM=$v",
+        if (args.type case final v?) "TYPE=$v",
+        if (args.version case final v?) "VERSION=$v",
+        if (args.vlan case final v when v || args.hostname is String) "VLAN=$v",
       ], "\n");
 
     final sink = file.openWrite()..write(buffer);
 
     await sink.close();
   }
+}
+
+class _InitArgs {
+  const _InitArgs(this._argResults);
+
+  final ArgResults? _argResults;
+
+  bool _flag(String name) => _argResults?.flag(name) ?? false;
+  List<String> _multiOption(String n) => _argResults?.multiOption(n) ?? [];
+  String? _option(String name) => _argResults?.option(name);
+
+  bool get force => _flag("force");
+
+  List<String> get apt => _multiOption("apt");
+  bool get autorestart => _flag("autorestart");
+  String? get avatar => _option("avatar");
+  String? get hostname => _option("hostname");
+  String? get id => _option("id");
+  String? get main => _option("main");
+  String? get name => _option("name");
+  String? get ram => _option("ram");
+  String? get type => _option("type");
+  String? get version => _option("version");
+  bool get vlan => _flag("vlan");
 }
