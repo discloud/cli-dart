936b44894772b6ddc76f4f6efc1ef2eebc12adab
Gorniaky <hwss.s2@gmail.com>
2026-03-29 17:30:15 -0300

    feat: add all options for init command

--- lib/commands/init.dart
+++ lib/commands/init.dart
@@ -3,14 +3,32 @@ import "dart:io";
 
 import "package:args/command_runner.dart";
 import "package:discloud/extensions/string.dart";
+import "package:discloud/services/discloud/constants.dart";
 import "package:discloud_config/discloud_config.dart";
 
 class InitCommand extends Command<void> {
   InitCommand() {
     argParser
-      ..addFlag("force", abbr: "f", negatable: false)
-      ..addOption("main")
-      ..addOption("type", allowed: const ["bot", "site"]);
+      ..addFlag(
+        "autorestart",
+        help:
+            "Determines whether the app should automatically restart if it crashes.",
+        negatable: false,
+      )
+      ..addFlag("force", abbr: "f", help: "Overwrite config file", negatable: false)
+      ..addFlag("vlan", help: "Enables private networking", negatable: false)
+      ..addMultiOption("apt", help: appApts.join(","))
+      ..addOption("avatar", help: "Image URL (.gif, .jpeg, .jpg, .png)")
+      ..addOption(
+        "hostname",
+        help: "Custom hostname alias for other apps to reach this one",
+      )
+      ..addOption("id", help: "User-defined subdomains")
+      ..addOption("main", help: "Relative file path")
+      ..addOption("name", help: "1 - 30 characters")
+      ..addOption("ram", help: "Amount in MB; min 100")
+      ..addOption("type", allowed: const ["bot", "site"])
+      ..addOption("version", help: "current|latest|legacy|suja|x.x.x");
   }
 
   @override
@@ -27,12 +45,29 @@ class InitCommand extends Command<void> {
       throw Exception("${DiscloudConfig.filename} already exists!");
     }
 
-    final sink = file.openWrite()
+    final args = argResults;
+
+    final hostname = args?.option("hostname");
+
+    final buffer = StringBuffer()
       ..writeAll([
         "# https://docs.discloud.com/en/discloud.config",
-        "MAIN=${argResults?.option("main").orEmpty}",
-        if (argResults?.option("type") case final type?) "TYPE=$type",
+        if (args?.multiOption("apt") case final v?) "APT=${v.join(",")}",
+        if (args?.flag("autorestart") case final v? when v) "AUTORESTART=$v",
+        if (args?.option("avatar") case final v?) "AVATAR=$v",
+        if (hostname case final v?) "HOSTNAME=$v",
+        if (args?.option("id") case final v?) "ID=$v",
+        "MAIN=${args?.option("main").orEmpty}",
+        if (args?.option("name") case final v?) "NAME=$v",
+        if (args?.option("ram") case final v?) "RAM=$v",
+        if (args?.option("type") case final v?) "TYPE=$v",
+        if (args?.option("version") case final v?) "VERSION=$v",
+        if (args?.flag("vlan") case final v?
+            when v || hostname is String && hostname.isNotEmpty)
+          "VLAN=$v",
       ], "\n");
+
+    final sink = file.openWrite()..write(buffer);
 
     await sink.close();
   }
