a4bf40fcf1e2680f60ebe0905468a5c6d23c4514
Gorniaky <30408913+Gorniaky@users.noreply.github.com>
2026-07-07 16:48:24 -0300

    Refactor option handling for app ID and glob

--- lib/commands/team/commit.dart
+++ lib/commands/team/commit.dart
@@ -39,17 +39,15 @@ final class TeamCommitCommand extends Command<void> with Disposable {
   Future<void> run() async {
     final directory = context.workspaceFolder;
 
-    final optionAppId = argResults!.option("app");
+    final optionAppId = argResults?.option("app");
     final appId =
         optionAppId ??
-        argResults!.optionOrRest("app") ??
+        argResults?.rest.firstOrNull ??
         await _getDiscloudConfigAppId(directory);
 
     if (appId == null) throw Exception("Missing app id");
 
-    final glob = optionAppId == null
-        ? argResults!.multiOptionOrRest("glob", ["app"]) ?? const ["**"]
-        : argResults!.multiOptionOrRest("glob") ?? const ["**"];
+    final glob = argResults?.multiOptionOrRest("glob", [if (optionAppId != null) "app"]) ?? const ["**"];
 
     final spinner = context.printer.spin(text: "Zipping...");
 
