e439dfb9fe8d75ab7cf2fa872aedb78e05ad2d0a
Gorniaky <hwss.s2@gmail.com>
2026-09-04 19:45:55 -0300

    refactor: integrate DirectoryExtension for directory recreation in generate script

--- scripts/docs/generate.dart
+++ scripts/docs/generate.dart
@@ -1,38 +1,23 @@
 // dart scripts/docs/generate.dart
 
 import "dart:io";
-import "dart:typed_data";
 
 import "package:args/command_runner.dart";
 import "package:discloud/cli/runner.dart";
 import "package:discloud/version.dart";
-import "package:path/path.dart";
 
+import "directory_extension.dart";
 import "generate/commands.dart";
 import "generate/index.dart";
 import "markdown_to_html_converter.dart";
 
 const _docsRootPath = "docs";
+const _filenamesToPreserve = {"_config.yml"};
 
 void main() async {
   final docRootDir = Directory(_docsRootPath);
 
-  const filesToPreserve = {"_config.yml"};
-  final filesToPreserveContents = <File, Uint8List>{};
-
-  for (final filename in filesToPreserve) {
-    final File file = .new(joinAll([_docsRootPath, filename]));
-    if (!await file.exists()) continue;
-
-    filesToPreserveContents[file] = await file.readAsBytes();
-  }
-
-  await docRootDir.delete(recursive: true);
-  await docRootDir.create(recursive: true);
-
-  for (final entry in filesToPreserveContents.entries) {
-    await entry.key.writeAsBytes(entry.value, flush: true);
-  }
+  await docRootDir.recreate(filenamesToPreserve: _filenamesToPreserve);
 
   const version = packageVersion == "0.0.0" ? "" : " v$packageVersion";
 
